113d4a616a9bf48377acefaff59c044b5c5f0b53
Leandro Simões <leandro.simoes@outlook.com>
2019-01-28 15:36:35 -0200

    add some bindings

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # ls-knockout-bindings
-Some useful Knockoutjs binding handlers
+Some simple but useful [Knockoutjs](https://knockoutjs.com) binding handlers
+
+## Binding Handlers
+
+`onpresskey` - Execute some action on press a key of your choice
+
+```javascript
+cosnt viewModel = {
+    onPressKeyOptions: {
+        key: 'ENTER',
+        action: (value, event) => {
+            // ... do anything you want
+        },
+        blurWhenPressed: false
+    }
+}
+```
+
+```html
+<input type="text" data-bind="onpresskey: onPressKeyOptions">
+```
+
+----------
+
+`range` - Limit a number input to a range of your choice
+
+```html
+<input type="number" data-bind="range: { min: 1, max: 5 }">
+```
+
+----------
+
+`hidden` - Knockoutjs has a `visible` binding. This is just a better way to negate that.
+
+```javascript
+cosnt viewModel = {
+    HasToHideThis: ko.observable(true)
+}
+```
+
+```html
+<div data-bind="hidden: HasToHideThis"></div>
+```
+
+----------
+
+`limitedInput` - Limit a input to accept just some keys and values based on a regex
+
+```html
+// For example, this input will accept just numbers
+<input type="text" data-bind="limitedInput: '[0-9]'">
+```
+
+----------
+
---
+++ src/js/index.js
@@ -0,0 +1,55 @@
+;
+(() => {
+    if (!ko)  throw new Error("Knockoutjs was not found. Please, see how to get it here: https://knockoutjs.com/downloads")
+
+    ko.bindingHandlers.onpresskey = {
+        init: (element, value, allbindings) => {
+            let { key = 'ENTER', action, blurWhenPressed = false } = ko.toJS(value())
+
+            if (typeof action !== 'function') throw new Error('Action must be a function');
+
+            element.addEventListener('keyup', event => {
+                if ((event.key || '').toUpperCase() === key) {
+                    action(ko.unwrap(allbindings().value), event)
+                    !!blurWhenPressed && event.currentTarget.blur()
+                }
+            })
+        }
+    }
+
+    ko.bindingHandlers.range = {
+        init: (element, model, allbindings) => {
+            let { min = 0, max = 100 } = model()
+            let { value } = allbindings()
+
+            if (!value) return
+
+            value.subscribe(v => {
+                if (isNaN(v) || parseInt(v) < min) {
+                    value(min);
+                } else if (parseInt(v) > max) {
+                    value(max);
+                }
+            })
+
+        }
+    }
+    
+    ko.bindingHandlers.hidden = {
+        update: (element, valueAccessor) => {
+            ko.bindingHandlers.visible.update(element, () => !ko.utils.unwrapObservable(valueAccessor()));
+        }
+    };
+
+    ko.bindingHandlers.validateBy = {
+        init: (element, valueAccessor) => {
+            let validationRegex = ko.utils.unwrapObservable(valueAccessor());
+
+            if (!validationRegex) throw new Error('invalid regex')
+
+            validationRegex = new RegExp(validationRegex, 'i');
+            
+            element.addEventListener('keypress', event => !!(event.key || '').match(validationRegex))
+        }
+    };
+})()
